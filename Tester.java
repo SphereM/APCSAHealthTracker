aeca42c5e4b0e5e7e02d879b46bdec6e3967899f
CreeptonThingz <creepton@outlook.com>
2021-03-28 21:12:46 -0600

    Add starting code for UI

--- Tester.java
+++ Tester.java
@@ -1,34 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
-public class Tester extends JFrame {
+public class Tester {
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            GUIManager manager = new GUIManager();
-            manager.setVisible(true);
-        });
-    }
-
-    public GUIManager() {
-        initUI();
-    }
-    
-    private void initUI() {
-        setPreferredSize(new Dimension(1280, 720));
-        setTitle("Health Tracker");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-    }
-
-    private void initHomeUI() {
-        var window = getContentPane();
-        var gl = new GroupLayout(window);
-        window.setLayout(gl);
-
-        gl.setAutoCreateGaps(true);
-        gl.setAutoCreateContainerGaps(true);
-
-
+        GUIManager manager = new GUIManager();
+        manager.start();
     }
 }
